1eaa73f823c49e8edb7be82f7f4b1163c17ac6f0
andresdanielmtz <autummata@gmail.com>
2024-10-09 17:25:41 -0600

    Add: Axios, Zustand and remaining libraries.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
 import Header from './components/Header'
+import useStore from "./createStore";
+
+interface AppState { 
+  count: number;
+}
 
 function App() {
+  const { count, inc } = useStore()
 
   return (
     <>
+    {count}
     <Header /> 
+
+    <button onClick={inc}>Increment</button>
     
     </>
   )
